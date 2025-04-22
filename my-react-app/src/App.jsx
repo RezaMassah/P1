d4a11a923737ca23989b6b557989e25cfb091663
unknown <massahr1983@gmail.com>
2025-04-22 17:36:52 -0500

    April 22nd

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,11 +3,24 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
-function App() {  // 👈 define the App component
+function App() {
+  const [inputValue, setInputValue] = useState('');
+
+  const change_the_content = (event) => {
+    setInputValue(event.target.value);
+  }
   return (
     <div>
       <p>
-        Click on the Vite and React logos to learn more about them and check with master
+        Please write a value here!
+      </p>
+      <input 
+      type='text'
+      value = {inputValue}
+      onChange={change_the_content}
+      />
+      <p>
+      Length: {inputValue.length}
       </p>
     </div>
   );
